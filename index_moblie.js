4390b6ffdafb46c89c68182f4ccf1c2dbad5632d
bigengbuchuo <993486146@qq.com>
2018-11-05 20:56:56 +0800

    Stopwatch_mobile
    新增了秒表时针旋转的动画、利用animation属性对时针变化动画做了延时处理。
    添加了秒表的次记功能。
    完成了所有静态界面、动画以及功能。

--- index_moblie.js
+++ index_moblie.js
@@ -1,6 +1,7 @@
 var pause = document.getElementsByClassName("pause")[0];
 var reset=document.getElementsByClassName("reset")[0];
 var needle=document.getElementsByClassName("needle")[0];
+var left=document.getElementsByClassName("left")[0];
 
 var flag = 1, f = 0;
 pause.addEventListener("touchstart", function () {
@@ -14,21 +15,31 @@ pause.addEventListener("touchstart", function () {
             time = document.getElementsByClassName("time")[0];
             show(time);
             needle.style.backgroundImage = "url(./src/img/pic3.png)";
+            needle.style.animation="mymove 3s linear infinite paused";
+            needle.style.animationPlayState="running";
         }, 10);
     } else {
         flag=1;
         f = value;
         clearInterval(timer);
         needle.style.backgroundImage = "url(./src/img/pic2.png)";
+        needle.style.animationPlayState="paused";
     }
 })
 
 reset.addEventListener("touchstart",function(){
     if(flag){
-       value=0; 
+       value=0;f=0;
        for(var i=0;i<5;i+=2){
         time.children[i].innerHTML="00";
        }
+       needle.style.animation="none";
+       if(countnum){
+        countnum=0;  
+        while(list.children[0]){
+            list.removeChild(list.children[0]);
+            }
+        }
     }
 })
 
@@ -48,3 +59,27 @@ function show(location) {  //传到哪
         location.children[i].innerHTML = digit[i / 2];
     }
 }
+
+var countnum=0;
+left.addEventListener("touchstart",function(){
+    countnum++;
+    var div = document.createElement("div");
+    console.log(div);
+    div.setAttribute("class","listwrap");
+    list=document.getElementsByClassName("list")[0];
+    list.insertBefore(div,list.children[0]);
+    var div = document.createElement("div");
+    listwrap=document.getElementsByClassName("listwrap")[0];
+    div.setAttribute("class","num");
+    listwrap.appendChild(div);
+    var div = document.createElement("div");
+    div.setAttribute("class","listime");
+    listwrap.appendChild(div);
+    var num=document.getElementsByClassName("num")[0];
+    num.innerHTML="Lap"+countnum;
+    var listime=document.getElementsByClassName("listime")[0];
+    listime.innerHTML=time.innerHTML;
+})
+
+
+
