18c3a3add9701686096d70d287cabb5ef58d3236
bigengbuchuo <993486146@qq.com>
2018-11-08 22:31:44 +0800

    报名页面pc
    改变了布局形式。

--- 报名页面pc/index.js
+++ 报名页面pc/index.js
@@ -1,5 +1,17 @@
 var button = document.getElementsByClassName("button")[0];
+var cardown= document.getElementsByClassName("cardown")[0];
+var left= document.getElementsByClassName("left")[0];
+var right= document.getElementsByClassName("right")[0];
 
 button.addEventListener("click",function(){
-    alert("确定提交？");
+    cardown.classList.add("newcardown");
+    left.style.filter="blur(30px)";
+    right.style.filter="blur(30px)";
+    for(var i=0;i<4;i++){
+        var div=document.createElement("div");
+        div.setAttribute("class","i");
+        var newcardown= document.getElementsByClassName("i")[0];
+        newcardown.appendChild(i);
+    }
 })
+
