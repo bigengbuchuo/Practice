75a29f741dafea71c4b867c422b6e5708916a893
bigengbuchuo <993486146@qq.com>
2018-11-19 17:47:40 +0800

    报名页面pc
    跑通了前后端，用一个新对象的属性值实现了数据的判断，把结果呈献给用户。var obj = JSON.parse(xhr.response);
                    alert(obj.message);

--- 报名页面pc/index.js
+++ 报名页面pc/index.js
@@ -11,7 +11,7 @@ button.addEventListener("click",function(){
     var newcardown= document.getElementsByClassName("newcardown")[0];
     newcardown.style.display="block";
     var input = document.getElementsByTagName("input");
-    var card3= document.getElementsByClassName("card3")[0];
+    card3= document.getElementsByClassName("card3")[0];
     for(var i=0;i<4;i++){
         var div=document.createElement("div");
         div.setAttribute("class","message");
@@ -40,18 +40,18 @@ button.addEventListener("click",function(){
         while(cardown.children[1]){
             cardown.removeChild(cardown.children[1]);
         }
-        for(var i=0;i<4;i++){
-            input[i].value="";
-        }
         newcardown.style.display="none";
         left.style.filter="blur(0)";
         right.style.filter="blur(0)";
         Ajax({
-            url: "http://join.xiyoumobile.com/signup/user/add",
+            url: "https://join.xiyoumobile.com/signup/user/add",
             type: "post",
-            data: "sid="+card3.children[2]+"&name="+card3.children[4]+"&clazz="+card3.children[6]+"&sex="+card3.children[8],
+            data: "sid="+card3.children[2].value+"&name="+card3.children[4].value+"&clazz="+card3.children[6].value+"&sex="+card3.children[8].value,
             async:true,  //是否异步
         });
+        for(var i=0;i<4;i++){
+            input[i].value="";
+        }
     })
 })
 
@@ -61,28 +61,31 @@ function Ajax(object){
         if(xhr.readyState==4){
             var status=xhr.status;
             if(status>=200&&status<300){
-                object.success(xhr.responseText);
+                var obj = JSON.parse(xhr.response);
+                alert(obj.message);
             }else{
-                object.fail(status);
+                alert("网络错误");
             }
         }
     })
-    var message=transfrom(object.data);
+    // var message=transfrom(object.data);
     if(object.type=="get"){
         xhr.open("get",object.url+"?"+message,object.async);
         xhr.send(null);
     }else if(object.type=="post"){
         xhr.open("post",object.url,object.async);
-        xhr.setRequestHeader("Authorization","Basic eGl5b3UzZzp4aXlvdTNnZnoxNTUqKi8v");
-        xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
-        xhr.send(message);
+        xhr.setRequestHeader("Authorization","Basic eGl5b3UzZzp4aXlvdTNnZnoxNTUqKi8v");//身份验证
+        xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");//
+        xhr.send(object.data);
     }
+   
 }
-function transfrom(data){
-    var arr=[];
-    for(thing in data){  //用thing来接收data的属性名
-        arr.push(encodeURIComponent(thing)+"="+encodeURIComponent(data[thing]));//访问对象的方式
-    }
-    return arr.join("&");
-}
+// function transfrom(data){
+//     console.log(data);
+//     var arr=[];
+//     for(thing in data){  //用thing来接收data的属性名
+//         arr.push(encodeURIComponent(thing)+"="+encodeURIComponent(data[thing]));//访问对象的方式
+//     }
+//     return arr.join("&");
+// }
 
